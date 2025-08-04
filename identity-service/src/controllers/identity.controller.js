1c4fe8dd0eca1dca7cdfe710d87a8fd95003b4e1
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-08-05 02:33:18 +0530

    feat(identity): add refresh token controller

--- identity-service/src/controllers/identity.controller.js
+++ identity-service/src/controllers/identity.controller.js
@@ -1,3 +1,4 @@
+const RefreshToken = require("../models/refreshToken.model.js");
 const User = require("../models/user.model.js");
 const generateTokens = require("../utils/generateToken.js");
 const logger = require("../utils/logger.js");
@@ -113,7 +114,81 @@ const loginUser = async (req, res) => {
   }
 };
 
+// Generate new refresh token
+const refreshTokenUser = async (req, res) => {
+  logger.info("Refresh token endpoint hit...");
+  try {
+    const { refreshToken } = req.body;
+
+    if (!refreshToken) {
+      logger.warn("Refresh token missing");
+
+      return res.status(400).json({
+        success: false,
+        message: "Refresh token missing",
+      });
+    }
+
+    const storedToken = await RefreshToken.findOne({ token: refreshToken });
+
+    if (!storedToken || storedToken.expiresAt < Date.now()) {
+      logger.warn("Invalid or expired refresh token.");
+
+      return res.status(401).json({
+        success: false,
+        message: "Invalid or expired refresh token.",
+      });
+    }
+
+    const user = await User.findById(storedToken.user);
+
+    if (!user) {
+      logger.warn("User not found.");
+
+      return res.status(404).json({
+        success: false,
+        message: "User not found.",
+      });
+    }
+
+    const { accessToken: newAccessToken, refreshToken: newRefreshToken } =
+      await generateTokens(user);
+
+    // Delete old refresh token
+    try {
+      const result = await RefreshToken.deleteOne({ _id: storedToken._id });
+      if (result.deletedCount === 0) {
+        logger.warn("Old refresh token could not be deleted");
+      }
+    } catch (tokenError) {
+      logger.error("Refresh token generation failed", tokenError);
+      return res.status(500).json({
+        success: false,
+        message: "Internal Server Error",
+      });
+    }
+
+    logger.info(
+      `Refresh token generated successfully for user ID: ${user._id}`
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "New tokens issued successfully.",
+      accessToken: newAccessToken,
+      refreshToken: newRefreshToken,
+    });
+  } catch (error) {
+    logger.error("Refresh token endpoint failed", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
 module.exports = {
   registerUser,
   loginUser,
+  refreshTokenUser,
 };
