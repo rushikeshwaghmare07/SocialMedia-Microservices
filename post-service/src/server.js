ed1e6cdb59ef692361823194089341c68487aa11
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-08-06 22:36:30 +0530

    feat(post): add winston logger setup

--- post-service/src/server.js
+++ post-service/src/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
+const logger = require("./utils/logger");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -14,16 +16,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.text());
 
 app.use((req, res, next) => {
-  console.log(`Received ${req.method} request to ${req.url}`);
-  console.log(`Request body: ${JSON.stringify(req.body)}`);
+  logger.info(`Received ${req.method} request to ${req.url}`);
+  logger.debug(`Request body: ${JSON.stringify(req.body)}`);
   next();
 });
 
 app.listen(PORT, () => {
-  console.log(`Post service is running on PORT: ${PORT}`);
+  logger.info(`Post service is running on PORT: ${PORT}`);
 });
 
 // Unhandled promise rejection
 process.on("unhandledRejection", (reason, promise) => {
-  console.log("Unhandled rejection at", promise, "reason", reason);
+  logger.error("Unhandled rejection at", promise, "reason", reason);
 });
